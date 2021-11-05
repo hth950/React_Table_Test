c5b6610ec0024a4d950e11d4e48b52f8d3336895
Hwang <hth950@gmail.com>
2021-11-05 17:12:15 +0900

    Test1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="background">
+      <img src={logo} lat="logo" />
+      <h2>Let's Test~</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Table() {
+    return(
+        <h3>
+            Hi
+        </h3>
+    )
+}
+
+export default Table;
